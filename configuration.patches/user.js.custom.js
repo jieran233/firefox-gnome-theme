bb927163bfbde7299cbc3810f572d95283714e15
jieran233 <jieranjiang233@gmail.com>
2024-10-21 05:17:16 +0800

    fork: add tabMinWidth, insertAfterCurrent in part user.js.custom for user.js

--- configuration.patches/user.js.custom.js
+++ configuration.patches/user.js.custom.js
@@ -1,5 +1,11 @@
 /************ Custom user.js ************/
 
+// Min width of tabs
+user_perf("browser.tabs.tabMinWidth", 128);
+
+// Insert new tab after current
+user_perf("browser.tabs.insertAfterCurrent", true);
+
 // Restore pinned tabs on demand
 user_perf("browser.sessionstore.restore_on_demand", true);
 user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
