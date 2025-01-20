9ad5d2616c00910247a6b72ca246c9881d135274
jieran233 <jieranjiang233@gmail.com>
2025-01-21 00:38:50 +0800

    fork: undo dom.ipc.processCount=1 again

--- configuration.patches/user.js.fast-firefox.override.js
+++ configuration.patches/user.js.fast-firefox.override.js
@@ -1,5 +1,5 @@
 /*** Override for fast-firefox ***/
-// user_pref("dom.ipc.processCount", 8);
-// user_pref("dom.ipc.processCount.webIsolated", 4);
+user_pref("dom.ipc.processCount", 8);
+user_pref("dom.ipc.processCount.webIsolated", 4);
 
 user_pref("dom.ipc.processPriorityManager.backgroundUsesEcoQoS", false);  // Disabling Efficiency Mode (for Windows 11)
