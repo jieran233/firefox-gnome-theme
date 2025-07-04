b90996306bfbe0c2f3005d65694f9add337ed245
jieran233 <jieranjiang233@gmail.com>
2025-07-04 11:46:25 +0800

    fork: change javascript.options.concurrent_multiprocess_gcs.cpu_divisor to 8 in user.js.ra1ahq.js

--- configuration.patches/user.js.ra1ahq.js
+++ configuration.patches/user.js.ra1ahq.js
@@ -4,7 +4,6 @@ user_pref("javascript.options.baselinejit.threshold", 50);
 user_pref("javascript.options.ion.threshold", 5000);
 user_pref("network.buffer.cache.size", 65535);
 
-// user_pref("javascript.options.concurrent_multiprocess_gcs.cpu_divisor", 8);
-user_pref("javascript.options.concurrent_multiprocess_gcs.cpu_divisor", 16);  // depends on user's system (nproc)
+user_pref("javascript.options.concurrent_multiprocess_gcs.cpu_divisor", 8);
 
 user_pref("browser.display.auto_quality_min_font_size", 0);  // 0 for modern PCs and 4k monitors, 1000 for weak computers
