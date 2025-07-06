5ecb43120bbc8f674738858d1636c94aa8f8a109
jieran233 <jieranjiang233@gmail.com>
2025-07-06 23:39:34 +0800

    fork: add sidebar-tweaks.css

--- scripts.fork/patch.py
+++ scripts.fork/patch.py
@@ -240,6 +240,8 @@ def main():
                                                                 target_dir="theme/fork")],
                     "theme.patches/fork/hide-tabs-toolbar.css": [lambda: patch_manager.patch_cp_file(source_path_or_url="theme.patches/fork/hide-tabs-toolbar.css",
                                                                 target_dir="theme/fork")],
+                    "theme.patches/fork/sidebar-tweaks.css": [lambda: patch_manager.patch_cp_file(source_path_or_url="theme.patches/fork/sidebar-tweaks.css",
+                                                                target_dir="theme/fork")],
                     "theme.patches/colors/cosmic-dark.css": [lambda: patch_manager.patch_cp_file(source_path_or_url="theme.patches/colors/cosmic-dark.css",
                                                                 target_dir="theme/colors")],
                     "theme.patches/icons/icons.css": [lambda: patch_manager.patch_cp_file(source_path_or_url="theme.patches/icons/icons.css",
